db3dc4899bedc6d65d162a5bb9fa2938181e0672
Andrea_SIS112 <andra.campero@ucb.edu.bo>
2024-08-21 18:08:31 -0400

    hackerrank

---
+++ practica.py
@@ -0,0 +1,24 @@
+#comentario 
+print("Hola mundo")
+"""dsadaad"""
+
+Var = 8
+print (type(Var))
+
+if (dato<=3):
+    print ("si es correcto ")
+else :
+    print("no es correcto")
+
+def suma (a,b):
+    return a+b
+
+a=5
+y=8
+
+suma(x,y)
+
+nombre=input("ingrese nombre")
+a= int(input("Ingrese el Numero"))   
+print(type(a))  
+
