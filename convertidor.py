0c73973cd0061e0a12c7a5759e43d04ad5c909fe
Andrea_SIS112 <andra.campero@ucb.edu.bo>
2024-09-11 19:00:42 -0400

    primerarchivoconvercion

---
+++ convertidor.py
@@ -0,0 +1,26 @@
+def longitudmetros(metros): 
+    return metros/1000
+def longitudkilometros(kilometros):
+    return kilometros*100
+def masagramos(gramos):
+    return gramos /1000
+def masakilogramos(kilogramos):
+    return kilogramos *1000
+def tiempocelcius(celcius):
+    return (celcius * 9/5)+32
+def tiempofahrenheit(fahrenheit):
+    return (fahrenheit -32)*5/9
+    
+    
+def mopcion(): #para escojer una opcion 
+    print("Elija una opcion: ")
+    print("1. longitud (metros <> kilometros )")
+    print("2. masa (gramos <> kilogramos )")
+    print("3. tiempo (metros <> tiempo )")
+    print("4. salir")
+    
+
+
+
+    
+
---
+++ diccionario.py
@@ -0,0 +1,6 @@
+#lista
+    lista=[]
+#diccionario
+    diccionario={"clave", "valor"}
+#Ejemplo
+EST{"estudiante_1":[100,100,100],"Estudiante_2":[0,0,0]}
