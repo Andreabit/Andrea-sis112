74113b7cd96040c9aa2c583986e80f600742eb18
Andrea_SIS112 <andra.campero@ucb.edu.bo>
2024-09-11 23:45:10 -0400

    segunda parte que funciona

--- archivomain.py
+++ archivomain.py
@@ -1,62 +1,42 @@
-def menu_longitud():
-    print("\nSeleccione una opción:")
-    print("1. Metros a kilómetros")
-    print("2. Kilómetros a metros")
-    opcion = input("Ingrese su elección: ")
+import convertidor
 
-    if opcion == '1':
-        metros = float(input("Ingrese los metros: "))
-        print(f"{metros} metros son {metroskilometros(metros)} kilómetros.")
-    elif opcion == '2':
-        kilometros = float(input("Ingrese los kilómetros: "))
-        print(f"{kilometros} kilómetros son {kilometrosmetros(kilometros)} metros.")
-    else:
-        print("Opción no válida.")
-
-def menu_masa():
-    print("\nSeleccione una opción:")
-    print("1. Gramos a kilogramos")
-    print("2. Kilogramos a gramos")
-    opcion = input("Ingrese su elección: ")
-
-    if opcion == '1':
-        gramos = float(input("Ingrese los gramos: "))
-        print(f"{gramos} gramos son {gramoskilogramos(gramos)} kilogramos.")
-    elif opcion == '2':
-        kilogramos = float(input("Ingrese los kilogramos: "))
-        print(f"{kilogramos} kilogramos son {kilogramosgramos(kilogramos)} gramos.")
-    else:
-        print("Opción no válida.")
-
-def menu_temperatura():
-    print("\nSeleccione una opción:")
-    print("1. Celsius a Fahrenheit")
-    print("2. Fahrenheit a Celsius")
-    opcion = input("Ingrese su elección: ")
-
-    if opcion == '1':
-        celsius = float(input("Ingrese los grados Celsius: "))
-        print(f"{celsius} grados Celsius son {celsiusfahrenheit(celsius)} grados Fahrenheit.")
-    elif opcion == '2':
-        fahrenheit = float(input("Ingrese los grados Fahrenheit: "))
-        print(f"{fahrenheit} grados Fahrenheit son {fahrenheitcelsius(fahrenheit)} grados Celsius.")
-    else:
-        print("Opción no válida.")
-
-# Programa principal
 while True:
-   
-    eleccion = input("Ingrese su elección: ")
-
-    if eleccion == '1':
-        menu_longitud()
-    elif eleccion == '2':
-        menu_masa()
-    elif eleccion == '3':
-        menu_temperatura()
-    elif eleccion == '4':
-        print("Saliendo del programa...")
+    opcion = convertidor.mostrar_menu()
+    opcion = int(input("Elija: "))
+    
+    if opcion == 1:
+        X = int(input("""1 para m a km
+        2 para km a m
+        :"""))
+        if X == 1:
+            metros = int(input("Ingresa cuantos metros convertir: "))
+            print("Tienes {} km".format(convertidor.longitudmetros(metros))) 
+        elif X == 2:
+            kilometros = int(input("Ingresa cuantos km convertir: "))
+            print("Tienes {} metros".format(convertidor.longitudkilometros(kilometros)))
+    
+    elif opcion == 2:
+        X = int(input("""1 para g a kg
+        2 para kg a g 
+        :"""))
+        if X == 1:
+            gramos = int(input("Ingrese cuantos gramos convertir: "))
+            print("Tienes {} kg".format(convertidor.masagramos(gramos)))
+        elif X == 2:
+            kilogramos = int(input("Ingrese cuantos kilogramos convertir: "))
+            print("Tienes {} g".format(convertidor.masakilogramos(kilogramos)))
+    
+    elif opcion == 3:
+        X = int(input("""1 para C° a F°
+        2 para F° a C°
+        :"""))
+        if X == 1:
+            celcius = int(input("Ingrese cuantos grados Celsius convertir: "))
+            print("Tienes {} °F".format(convertidor.tiempocelcius(celcius)))
+        elif X == 2:
+            fahrenheit = int(input("Ingrese cuantos grados Fahrenheit convertir: "))
+            print("Tienes {} °C".format(convertidor.tiempofahrenheit(fahrenheit)))
+    
+    elif opcion == 4:
+        print("Saliendo...")
         break
-    else:
-        print("Opción no válida.")
-        
